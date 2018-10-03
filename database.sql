0c3c6a3bd1733d11ec67872f2cb5ea30c7fe40c4
stpaulsschool420 <43798324+stpaulsschool420@users.noreply.github.com>
2018-10-03 20:02:19 +0000

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,11 @@
+
+/* After installing MySQL, the first thing we need to do is create a
+ * database.
+ *
+ * mysql> SOURCE ~/code/Learn-SQL-The-Hard-Way/Exercise-0-The-Setup/create_database.sql
+ */
+
+-- Create the database
+CREATE DATABASE learn_sql_the_hard_way;
+
+SHOW DATABASES;
